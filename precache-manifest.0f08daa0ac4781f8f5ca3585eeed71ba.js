ba891cab5a1b1ed1f852c88c841598abc7c3ee7e
Rushil <rushilbhatt533@gmail.com>
2021-03-19 19:57:01 +0100

    Updates

---
+++ precache-manifest.0f08daa0ac4781f8f5ca3585eeed71ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b57c5e3784adc16a5e2a786b323e4a3",
+    "url": "/covid__tracker/index.html"
+  },
+  {
+    "revision": "df891cabf77b35cb3abe",
+    "url": "/covid__tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "c3884af9b64a068c0b4b",
+    "url": "/covid__tracker/static/css/main.915e0b82.chunk.css"
+  },
+  {
+    "revision": "df891cabf77b35cb3abe",
+    "url": "/covid__tracker/static/js/2.799d00aa.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covid__tracker/static/js/2.799d00aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3884af9b64a068c0b4b",
+    "url": "/covid__tracker/static/js/main.9e4c8e76.chunk.js"
+  },
+  {
+    "revision": "18d30736885f6d6ee8fe",
+    "url": "/covid__tracker/static/js/runtime-main.fe63e138.js"
+  }
+]);
